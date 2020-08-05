3d121646a4f137a4b6950d7999392acee0990254
缪宇 <1337611267@qq.com>
2020-08-05 16:45:50 +0800

    🐛修复连线消失问题

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,11 @@ import Port from "./components/Port";
 export default function App() {
   return (
     <Box className="App" id="js_container">
-      <svg width="190" height="160" xmlns="http://www.w3.org/2000/svg">
-        {/* <path d="M 10 80 C 40 10 , 65 10, 95 80 S 150 150, 180 80" stroke="black" fill="transparent"/> */}
-        {/* <path d="M 10 80 Q 40 10 , 95 80 T 180 80" stroke="black" fill="transparent"/> */}
-        {/* <path d="M 0, 80 Q 95 80, 95 80 T 190 80" stroke="black" fill="transparent"/> */}
-      </svg>
+      {/* <svg width="2" height="160" xmlns="http://www.w3.org/2000/svg"> */}
+      {/* <path d="M 10 80 C 40 10 , 65 10, 95 80 S 150 150, 180 80" stroke="black" fill="transparent"/> */}
+      {/* <path d="M 10 80 Q 40 10 , 95 80 T 180 80" stroke="black" fill="transparent"/> */}
+      {/* <path d="M 0, 80 Q 95 80, 95 80 T 190 80" stroke="black" fill="transparent"/> */}
+      {/* </svg> */}
       <Port />
     </Box>
   );
